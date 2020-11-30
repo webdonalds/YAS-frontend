577998f9906bbee7e365ab04d8e2a40747ead91d
hywook4 <hywook4@gmail.com>
2020-11-30 23:38:32 +0900

    Fix css problem

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./auth/Login";
 
+import "./Main.css";
+
 const Main: React.FC = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={()=>(<div />)} />
-      <Route path="/login" component={Login} />
-      {/* TODO: other path handling */}
-    </Switch>
-  </BrowserRouter>
+  <div className="full_main_page">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={()=>(<div />)} />
+        <Route path="/login" component={Login} />
+        {/* TODO: other path handling */}
+      </Switch>
+    </BrowserRouter>
+  </div>
+  
 );
 
 export default Main;
