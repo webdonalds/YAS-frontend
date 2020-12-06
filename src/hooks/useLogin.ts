1e242a654acf8befbe06a76de0bd26d8f80778f7
hywook4 <hywook4@gmail.com>
2020-12-06 15:28:49 +0900

    Add persistent logged in feature

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,12 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules';
-import { loginThunk } from '../modules/auth/authThunk';
+import { getSavedLoginThunk, loginThunk } from '../modules/auth/authThunk';
+import _ from 'lodash';
 import { useCallback } from 'react';
+import localStorageService from '../service/localStorageService';
 
 const useLogin = () => {
   const { userInfo, bearerToken, error } = useSelector((state: RootState) => state.auth);
+
   const dispatch = useDispatch();
 
+  // check if saved login exist
+  const savedUserLoginInfo: userLoginInfo = localStorageService.getUserLoginInfoFromLocalStorage();
+
+  if(savedUserLoginInfo != null){
+    if(!(_.isEqual(userInfo, savedUserLoginInfo.userInfo) && bearerToken===savedUserLoginInfo.token)){
+      dispatch(getSavedLoginThunk(savedUserLoginInfo));
+    }
+  }
+  
   const onLogin = useCallback((code) => dispatch(loginThunk(code)), [dispatch])
 
 
--- src/modules/auth/authThunk.ts
+++ src/modules/auth/authThunk.ts
@@ -2,6 +2,7 @@ import { ThunkAction } from 'redux-thunk';
 import { RootState } from '..';
 import { AuthAction, loginRequest, loginSuccess, loginError } from './auth';
 import { getAuthToken } from '../../api/login';
+import localStorageService from '../../service/localStorageService';
 
 const loginThunk = (code: string): ThunkAction<void, RootState, null, AuthAction> => {
   return async (dispatch) => {
@@ -9,6 +10,13 @@ const loginThunk = (code: string): ThunkAction<void, RootState, null, AuthAction
 
     try {
       const loginInfo = await getAuthToken(code);
+
+      // save logged in to local storage
+      localStorageService.setUserLoginInfoToLocalStorage({
+        userInfo: loginInfo.userInfo,
+        token: loginInfo.token
+      });
+
       dispatch(loginSuccess(loginInfo.userInfo, loginInfo.token));
     } catch (e) {
       dispatch(loginError(e));
@@ -16,6 +24,15 @@ const loginThunk = (code: string): ThunkAction<void, RootState, null, AuthAction
   }
 }
 
+
+const getSavedLoginThunk = (loginInfo: userLoginInfo): ThunkAction<void, RootState, null, AuthAction> => {
+  return async (dispatch) => {
+    console.log("111");
+    dispatch(loginSuccess(loginInfo.userInfo, loginInfo.token));
+  }
+}
+
 export {
   loginThunk,
+  getSavedLoginThunk
 };
