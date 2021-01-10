756fe39258d5f4a8d36d7708c437ec317a518c52
hywook4 <hywook4@gmail.com>
2021-01-10 15:06:31 +0900

    Add api for home post lists

---
+++ src/api/home.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export type videoPostInfo = {
+    id: number,
+    videoId: string,
+    userId: number,
+    title: string,
+    description: string,
+    totalLikes: number,
+    createdAt: string,
+    updatedAt: string
+};
+
+type videoPostsResponse = {
+    videoList: Array<videoPostInfo>,
+    pageToken: number|null
+}
+
+const getRecentVideoPosts = async (pageToken:number|null): Promise<videoPostsResponse> => {
+    const res = await axios.request<videoPostsResponse>({
+        baseURL: API_URL,
+        url: '/v1/post-list/recent-videos',
+        method: 'get',
+        params: {
+            pageToken: pageToken
+        }
+    });
+    return res.data;
+}
+
+
+const getHotVideoPosts = async (): Promise<videoPostsResponse> => {
+    const res = await axios.request<videoPostsResponse>({
+        baseURL: API_URL,
+        url: '/v1/post-list/hot-videos',
+        method: 'get'
+    });
+    return res.data;
+}
+
+
+export {
+  getRecentVideoPosts,
+  getHotVideoPosts
+}; 
