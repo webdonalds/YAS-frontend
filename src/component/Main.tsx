8b6d2b6d03ab66a6ff1097e70c1560d9fb5fdf89
zych1751 <zych1751@naver.com>
2021-01-20 22:55:13 +0900

    get token from localstorage only once when loading main component

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import GetLogin from "../hooks/GetLogin";
+import { getSavedLoginThunk } from "../modules/auth/authThunk";
+import localStorageService from "../service/localStorageService";
 import Add from "./content/Add/AddVideo";
 import Header from "./content/Header/Header";
 import Home from "./content/Home/Home";
@@ -7,6 +11,27 @@ import Home from "./content/Home/Home";
 import "./Main.css";
 
 const Main: React.FC = () => {
+  const { userInfo, tokens, error } = GetLogin();
+  const [initialized, setInitialized] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const initialize = () => {
+    // if not initialized
+    if(!initialized) {
+      if(userInfo==null && tokens==null && error==null) {
+        const savedUserLoginInfo = localStorageService.getUserLoginInfoFromLocalStorage();
+
+        if(savedUserLoginInfo!=null){
+          dispatch(getSavedLoginThunk(savedUserLoginInfo));
+        }
+      }
+      setInitialized(true);
+    }
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(initialize, [initialized]);
+
   return (
     <BrowserRouter>
       <div className="full_main_page container">
