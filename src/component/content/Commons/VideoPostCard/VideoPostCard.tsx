fc7afe2bf698b93b4a0136b171404d92b9a4124a
hywook4 <hywook4@gmail.com>
2021-02-28 21:47:59 +0900

    Apply links to video cards and some buttons

--- src/component/content/Commons/VideoPostCard/VideoPostCard.tsx
+++ src/component/content/Commons/VideoPostCard/VideoPostCard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
-import { FaTag, FaThumbsUp } from "react-icons/fa";
-import utils from "../../../../service/utils"
+import { FaTag, FaThumbsUp, FaEdit, FaTrash } from "react-icons/fa";
+import utils from "../../../../service/utils";
+import GetLogin from "../../../../hooks/GetLogin";
 
 import "./VideoPostCard.css";
 
 
 const VideoPostCard: React.FC<VideoPostInfoWithUser> = ( videoPost ) => {
+  const { userInfo } = GetLogin();
 
   const videoThumbnailUrl = utils.getYoutubeThumbnailUrl(videoPost.videoId);
   
@@ -23,20 +26,34 @@ const VideoPostCard: React.FC<VideoPostInfoWithUser> = ( videoPost ) => {
     </Card.Text>
   );
 
+  const ownerButtons = (
+    <Card.Text>
+      <Link to={"/modify-video/" + videoPost.id}>
+        <FaEdit/>
+      </Link>
+      <FaTrash/>
+    </Card.Text>
+  )
+
 
   return (
-    <Card style={{ width: '18rem', flexDirection: 'row' }}>
-      <Card.Img variant="top" src={videoThumbnailUrl}/>
-      <Card.Body style={{width: "100rem"}}>
-        <Card.Title>{videoPost.title} - post by {videoPost.user.nickname}</Card.Title>
-        <Card.Body>
-          <Card.Text>{videoPost.description}</Card.Text>
-          <Card.Text>{videoPost.createdAt}</Card.Text>
-          <Card.Text><FaThumbsUp/>{videoPost.totalLikes}</Card.Text>
-          {tagsView}
+    <Link to={"/video/" + videoPost.id} style={{ textDecoration: 'none', color: 'black'}}>
+      <Card style={{ width: '18rem', flexDirection: 'row' }}>
+        <Card.Img variant="top" src={videoThumbnailUrl}/>
+        <Card.Body style={{width: "100rem"}}>
+          <Card.Title>{videoPost.title} - post by {videoPost.user.nickname}</Card.Title>
+          <Card.Body>
+            <Card.Text>{videoPost.description}</Card.Text>
+            <Card.Text>{videoPost.createdAt}</Card.Text>
+            <Card.Text><FaThumbsUp/>{videoPost.totalLikes}</Card.Text>
+            {tagsView}
+          </Card.Body>
         </Card.Body>
-      </Card.Body>
-    </Card>
+        <Card.Body>
+          {userInfo && userInfo.id == videoPost.userId ? ownerButtons : null }
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
 
