7924f95a6192f47aecfeeaf4f8629cbe0daff6db
hywook4 <hywook4@gmail.com>
2021-03-29 00:36:44 +0900

    New video card design

--- src/component/content/Commons/VideoPostCard/VideoPostCard.tsx
+++ src/component/content/Commons/VideoPostCard/VideoPostCard.tsx
@@ -9,14 +9,14 @@ const VideoPostCard: React.FC<VideoPostInfoWithUser> = ( videoPost ) => {
   
   const getTagView = (tag: string, idx: number) => {
     return (
-      <span key={idx} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+      <span key={idx} className="inline-block bg-gray-200 rounded-full px-1 py-0.5 text-xs font-semibold text-gray-700 mr-1">
         #{tag}
       </span>
     )
   };
 
   const tagsView = (
-    <div className="px-6 pt-4 pb-2">
+    <div className="pt-2">
       {videoPost.tags.map((tag, idx) => getTagView(tag.tagName, idx))}
     </div>
   );
@@ -25,13 +25,19 @@ const VideoPostCard: React.FC<VideoPostInfoWithUser> = ( videoPost ) => {
     <Link to={"/video/" + videoPost.id} style={{ textDecoration: 'none', color: 'black'}}>
       <div className="rounded overflow-hidden shadow-lg">
         <img className="w-full" src={videoThumbnailUrl}/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{videoPost.title}</div>
-          <p className="text-gray-700 text-base">
-            {videoPost.description}
-          </p>
+        <div className="px-2 py-2 h-44">
+          <div className="inline-block align-top w-2/12 mr-1">
+            <img className="rounded-full h-10 w-10 object-cover cursor-pointer" src={videoPost.user.imagePath  ? videoPost.user.imagePath : utils.defaultProfileImage}/>
+          </div>
+          <div className="inline-block align-top w-9/12">
+            <div className="text-base font-semibold inline-block max-h-10 overflow-hidden w-full leading-tight">{videoPost.title}</div>
+            <div className="text-gray-700 text-sm font-semibold">
+              <p>{videoPost.user.nickname}</p>
+              <p>{videoPost.createdAt.toString().split('T')[0]} &#183; {videoPost.totalLikes} likes </p>
+            </div>
+          </div>
+          {tagsView}
         </div>
-        {tagsView}
       </div>
     </Link>
   );
--- src/component/content/Home/Home.tsx
+++ src/component/content/Home/Home.tsx
@@ -88,7 +88,7 @@ const Home: React.FC<RouteComponentProps> = () => {
         </Nav.Item>
       </Nav>
 
-      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-3">
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
         {
           videoPostsState.videoPosts.map(post => (
             // TODO: tags 넣기
