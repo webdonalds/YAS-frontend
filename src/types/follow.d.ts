77d0c125c8033e0e6ee221e2c59d65f24d002581
hywook4 <hywook4@gmail.com>
2021-08-08 23:01:47 +0900

    Add new types for new follow APIs

--- src/types/follow.d.ts
+++ src/types/follow.d.ts
@@ -9,3 +9,16 @@ declare type FollowListResponse = {
   follows: Array<FollowInfo>,
   pageToken: number|null
 }
+
+declare type IsFollowingResponse = {
+  isFollowing: boolean
+}
+
+declare type PostFollowResponse = {
+  followerId: number,
+  followeeId: number
+}
+
+declare type DeleteFollowResponse = {
+  message: string
+}
